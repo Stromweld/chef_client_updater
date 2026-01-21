6d86f6380d133c421984aef5b062c250e6f2da4c
Corey Hemminger <hemminger@hotmail.com>
2026-01-20 18:55:32 -0600

    test
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/cookbooks/test/recipes/license_id.rb
+++ test/cookbooks/test/recipes/license_id.rb
@@ -1,7 +1,7 @@
 chef_client_updater "Install Chef #{node['chef_client_updater']['version']}" do
   channel 'stable'
   version node['chef_client_updater']['version']
-  post_install_action 'exec'
+  # post_install_action 'exec'
   install_timeout 599
   license_id node['chef_client_updater']['license_id'] if node['chef_client_updater']['license_id']
 end
